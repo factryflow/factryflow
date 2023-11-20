c0c190859b66f59985f29edd6f1db6cd9dcf6df5
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-11-20 10:17:50 +0000

    remover user views

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,1 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("login/", views.login, name="login"),
-    # path("register/", views.Logout.as_view(), name="register"),
-]
+urlpatterns = []
