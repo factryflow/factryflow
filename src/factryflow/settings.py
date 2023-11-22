e3edfcfd646159bb48b906c89d002dcf4517a3f3
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-11-22 09:40:43 +0000

    fix settings.py

--- src/factryflow/settings.py
+++ src/factryflow/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
-    "common"
+    "common",
     "django_htmx",
     "template_partials",
     "allauth",
