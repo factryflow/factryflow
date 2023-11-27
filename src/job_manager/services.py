a84301b82cb6dc0f2452f20388f07a77969c4209
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-11-27 11:24:55 +0000

    add simple job services

---
+++ src/job_manager/services.py
@@ -0,0 +1,27 @@
+from common.utils.service_helpers import create_or_update_model
+from django.db import transaction
+from job_manager.models.job import Job, JobStatus, JobType
+
+
+@transaction.atomic
+def job_type_create(*, name: str) -> JobType:
+    job_type = JobType(name=name)
+    job_type.full_clean()
+    job_type.save()
+
+    return job_type
+
+
+@transaction.atomic
+def job_create_or_update(
+    *, job_data: dict, job_type: JobType, job_status: JobStatus
+) -> Job:
+    job = create_or_update_model(Job, job_data)
+
+    job.job_type = job_type
+    job.job_status = job_status
+
+    job.full_clean()
+    job.save()
+
+    return job
