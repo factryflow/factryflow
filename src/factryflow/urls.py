4c8df8bba9843bb41f1355f62d0089970b35ddde
manish <mytechworker@gmail.com>
2023-12-06 18:05:54 +0530

    add task index and form page

--- src/factryflow/urls.py
+++ src/factryflow/urls.py
@@ -9,13 +9,21 @@ def home(request):
 def job(request):
     return render(request,"base/job/main.html")
 
+def task(request):
+    return render(request,"base/task/main.html")
+
 def jobform(request):
     return render(request,"base/job/form.html")
+
+def taskform(request):
+    return render(request,"base/task/form.html")
 
 urlpatterns = [
     path("", home, name="home"),
     path("job/",job, name="job"),
     path("job/form/",jobform, name="jobform"),
+    path("task/",task,name="task"),
+    path("task/form/",taskform,name="taskform"),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("users.urls")),
