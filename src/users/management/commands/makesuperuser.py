0fe5861804f33591ff14eeb35b735f17c334665c
Ujjwal Gusain <ujjwalgusain51@gmail.com>
2023-12-14 19:04:20 +0530

    added custom command to create superuser using env variables

---
+++ src/users/management/commands/makesuperuser.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    """
+    CUSTOM COMMAND
+    --------------
+        To create superuser using environment variables
+
+    ENV VARIABLES
+    -------------
+        SUPERUSER_USERNAME: str - email or username
+        SUPERUSER_PASSWORD: str - password for account
+    """
+    help = "Command to create a superuser"
+
+    def handle(self, *args, **options):
+        username = settings.SUPERUSER_USERNAME
+        password = settings.SUPERUSER_PASSWORD
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username, password=password, is_active=True
+            )
+            msg = self.style.SUCCESS(f"Admin with username: {username} was created")
+        else:
+            msg = self.style.NOTICE(f"Admin with username: {username} already exists")
+        self.stdout.write(msg)
