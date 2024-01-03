86954c094dff098e96bbd8def5d42a02de16f993
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2024-01-03 14:40:02 +0000

    add exception_handler for 404

--- src/api/api.py
+++ src/api/api.py
@@ -6,16 +6,17 @@ from django.core.exceptions import (
     PermissionDenied,
     ValidationError,
 )
+from django.http import Http404
+from job_manager.api import job_manager_router
 from ninja import NinjaAPI
 from ninja.errors import ValidationError as NinjaValidationError
 from ninja.security import APIKeyHeader
-
+from resource_assigner.api import resource_assigner_router
 
 # import and register routers
 from resource_calendar.api import resource_calendar_router
-from resource_assigner.api import resource_assigner_router
 from resource_manager.api import resource_manager_router
-from job_manager.api import job_manager_router
+
 
 class ApiKey(APIKeyHeader):
     param_name = "X-API-Key"
@@ -43,6 +44,15 @@ def handle_object_does_not_exist(request, exc):
     return api.create_response(
         request,
         {"message": "ObjectDoesNotExist", "detail": str(exc)},
+        status=HTTPStatus.NOT_FOUND,
+    )
+
+
+@api.exception_handler(Http404)
+def handle_404_error(request, exc: Http404):
+    return api.create_response(
+        request,
+        data={"message": "ObjectNotFound", "detail": str(exc)},
         status=HTTPStatus.NOT_FOUND,
     )
 
