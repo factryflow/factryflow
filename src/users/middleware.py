01b9b571b602f952e62ac91d6598fb42cacf079d
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2024-01-05 08:55:49 +0000

    update middelware to allow access to api

--- src/users/middleware.py
+++ src/users/middleware.py
@@ -5,17 +5,22 @@ from django.utils.deprecation import MiddlewareMixin
 
 class LoginRequiredMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
-        # Define paths that don't require authentication
-        exempt_paths = [
-            reverse("account_login"),
-            reverse("account_signup"),
-            reverse("account_reset_password"),
-            reverse("account_reset_password_done"),
-            reverse("api"),
-        ]
+        # Define view names that don't require authentication
+        exempt_view_names = (
+            "account_login",
+            "account_signup",
+            "account_reset_password",
+            "account_reset_password_done",
+        )
 
-        # Skip middleware for exempt paths
-        if request.path in exempt_paths or request.user.is_authenticated:
+        # Define paths that should bypass the login requirement
+        exempt_paths = ("/api/",)
+
+        # Convert view names to URLs
+        exempt_urls = [reverse(view_name) for view_name in exempt_view_names]
+
+        # Skip middleware for exempt paths and any path starting with /api/
+        if any(request.path_info.startswith(path) for path in exempt_urls + list(exempt_paths)) or request.user.is_authenticated:
             return None
 
         # Use login_required for other views
