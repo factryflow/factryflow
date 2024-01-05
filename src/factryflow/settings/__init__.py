d2c0665264706e9a4719a6a46e0341ddf31738ff
ujjwal51 <ujjwalgusain51@gmail.com>
2024-01-05 11:15:45 -0500

    env issue resolved

--- src/factryflow/settings/__init__.py
+++ src/factryflow/settings/__init__.py
@@ -1,9 +1,9 @@
-from os import environ
+import os
 
 from split_settings.tools import include
 
 # Managing environment via `DJANGO_ENV` variable:
-ENV = environ["DJANGO_ENV"]
+ENV = os.getenv("DJANGO_ENV", "development")
 
 if ENV not in ["development", "production"]:
     raise ValueError("Incorrect DJANGO_ENV value: {0}".format(ENV))
