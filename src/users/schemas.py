9e33160feac0d845e2eb904152f16617f5408340
ujjwal51 <ujjwalgusain51@gmail.com>
2024-01-12 16:43:04 +0530

    fix: git conflict issues

---
+++ src/users/schemas.py
@@ -0,0 +1,12 @@
+from enum import Enum
+from ninja import Schema
+
+class RoleChoices(str, Enum):
+    Admin = "admin"
+    Operator = "operator"
+    Planner = "planner"
+    ReadOnly = "read_only"
+
+class RoleIn(Schema):
+    user_id: int
+    name: RoleChoices
