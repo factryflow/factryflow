d820c280aaeb9ea8acfd3596e694d4853e65f836
Riddick <rdn@oestergaard-as.dk>
2024-05-28 22:38:37 +0800

    feat: devcontainer support

--- Makefile
+++ Makefile
@@ -1,24 +1,24 @@
 dev:
 	@echo "Starting Development Server..."
-	python manage.py runserver 0.0.0.0:8000
+	python src/manage.py runserver 0.0.0.0:8000
 
 migrate:
-	python manage.py migrate
+	python src/manage.py migrate
 
 migrations:
-	python manage.py makemigrations
+	python src/manage.py makemigrations
 
 test:
 	@cd src && pytest -W ignore
 
 superuser:
-	python manage.py makesuperuser
+	python src/manage.py makesuperuser
 
 sync_roles:
-	python manage.py sync_roles
+	python src/manage.py sync_roles
 
 create_user:
-	python manage.py createuser
+	python src/manage.py createuser
 
 add_data:
-	python manage.py add_data
+	python src/manage.py add_data
--- src/factryflow/settings/environments/development.py
+++ src/factryflow/settings/environments/development.py
@@ -1,13 +1,14 @@
 # All settings related to development environment goes here.
 import os
 
-from factryflow.settings.components.common import DEBUG, MIDDLEWARE, INSTALLED_APPS
+from factryflow.settings.components.common import DEBUG, MIDDLEWARE, INSTALLED_APPS, BASE_DIR
 
 # CUSTOM AUTH MIDDLEWARE SETTINGS
 AUTH_MIDDEWARE = [
     "users.middleware.LoginRequiredMiddleware",
 ]
 
+DEBUG = os.getenv("DEBUG") == "TRUE"
 DISABLE_AUTH = os.getenv("DISABLE_AUTH") == "TRUE"
 
 if not (DEBUG and DISABLE_AUTH):
@@ -27,3 +28,32 @@ if DEBUG:
     DEBUG_TOOLBAR_CONFIG = {
         "INTERCEPT_REDIRECTS": False,
     }
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR, "templates", "src/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+            "builtins": ["template_partials.templatetags.partials"],
+        },
+    },
+]
+
+# postgres database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
