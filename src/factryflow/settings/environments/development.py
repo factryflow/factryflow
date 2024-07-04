d8b7f539698ae3116e8a5d7f4d6b20730b7e0b7c
Riddick <rdn@oestergaard-as.dk>
2024-07-04 18:24:48 +0800

    fix: TEMPORARY FIX FOR DATABASE GitHub pipeline issue

--- src/factryflow/settings/environments/development.py
+++ src/factryflow/settings/environments/development.py
@@ -52,13 +52,13 @@ TEMPLATES = [
 ]
 
 # postgres database
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
