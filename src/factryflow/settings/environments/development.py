f03286e80f99100a98282d3b708e3c14dcdc8a90
MJ Ducut <mjd@oestergaard-as.dk>
2024-07-05 15:50:31 +0800

    CI/CD updates

--- src/factryflow/settings/environments/development.py
+++ src/factryflow/settings/environments/development.py
@@ -51,14 +51,14 @@ TEMPLATES = [
     },
 ]
 
-# postgres database
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+if os.getenv("DATABASE") == "postgres":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
     }
-}
