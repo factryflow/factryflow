9bed471d6851d70fd3a14d2cef8679408cc238dd
Avinash <avinashalanjkar@gmail.com>
2024-07-10 16:52:18 +0530

    fix - test case fixed for assignment constraints

--- src/resource_assigner/models.py
+++ src/resource_assigner/models.py
@@ -1,4 +1,4 @@
-from pydantic import ValidationError
+from django.core.exceptions import ValidationError
 from common.models import BaseModel, BaseModelWithExtras
 from django.db import models
 from job_manager.models import Task, WorkCenter
