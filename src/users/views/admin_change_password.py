6f41d4898feeed43753a08304e7592dff0aedb98
Avinash <avinashalanjkar@gmail.com>
2024-09-10 13:01:57 +0530

    fix: error handling for users change password

--- src/users/views/admin_change_password.py
+++ src/users/views/admin_change_password.py
@@ -9,11 +9,32 @@ from users.services import UserService
 
 
 class AdminChangePasswordView(FormView):
+    """
+    Handle POST request to create or update a model instance using Django form and service.
+
+    Args:
+        request: The HTTP request object.
+        id: The ID of the model instance (optional).
+
+    Returns:
+        The response indicating success or failure of the operation.
+    """
     template_name = "account/admin_change_password.html"
     form_class = AdminChangePasswordForm
 
     def post(self, request, id):
         form = self.get_form()
+
+        if len(form.errors) > 0:
+            form_errors = []
+            for _, errors in form.errors.items():
+                for error in errors:
+                    form_errors.append({
+                        "type": "error",
+                        "success": False,
+                        "message": error,
+                    })
+            return render(self.request, self.template_name, {"form": form, "form_errors": form_errors})
 
         if form.is_valid():
             user = get_object_or_404(User, id=id)
