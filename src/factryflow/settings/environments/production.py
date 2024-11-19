d01d2b527ce486ba9a20441f23c216c08f19916c
MJ Ducut <mjd@oestergaard-as.dk>
2024-11-19 14:01:55 +0800

    Updated production database based on env variable

--- src/factryflow/settings/environments/production.py
+++ src/factryflow/settings/environments/production.py
@@ -9,17 +9,18 @@ MIDDLEWARE += [
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-# postgres database
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+if os.getenv("DATABASE") == "postgres":
+    # postgres database
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
     }
-}
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
