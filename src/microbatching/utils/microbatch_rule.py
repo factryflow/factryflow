2e52f2e9e075e8ff7b95d49b88cada6379239ade
Avinash <avinashalanjkar@gmail.com>
2024-12-05 14:40:18 +0530

    fix: nested criteria create issue and updates related to matching task rule based on nested criteria

--- src/microbatching/utils/microbatch_rule.py
+++ src/microbatching/utils/microbatch_rule.py
@@ -1,10 +1,12 @@
-from common.utils.services import check_criteria_match
+from django.contrib.contenttypes.models import ContentType
 
 from microbatching.models.microbatch_rule import (
     MicrobatchRule,
-    MicrobatchRuleCriteria,
     MicrobatchRuleTaskMatch,
 )
+from job_manager.models import Task
+from common.models import NestedCriteriaGroup
+from common.utils.criteria import build_nested_query
 
 
 def create_microbatch_rule_matches(tasks) -> list:
@@ -20,18 +22,27 @@ def create_microbatch_rule_matches(tasks) -> list:
 
             if microbatch_rules:
                 for rule in microbatch_rules:
-                    # get all rule criteria for the rule
-                    rule_criteria = MicrobatchRuleCriteria.objects.filter(
-                        microbatch_rule=rule
-                    )
+                    # get the root NestedCriteriaGroup for the rule
+                    try:
+                        root_group = NestedCriteriaGroup.objects.get(
+                            content_type=ContentType.objects.get_for_model(
+                                MicrobatchRule
+                            ),
+                            object_id=rule.id,
+                            parent_group=None,
+                        )
+                    except NestedCriteriaGroup.DoesNotExist:
+                        continue
 
-                    criteria_match = False
-                    if rule_criteria:
-                        # check if any rule criteria matches the task
-                        for criteria in rule_criteria:
-                            if check_criteria_match(task, criteria):
-                                criteria_match = True
-                                matching_task_count += 1
+                    # # build a nested query for the root group
+                    query = build_nested_query(root_group)
+
+                    # check if the task matches the query
+                    if query and Task.objects.filter(query, id=task.id).exists():
+                        criteria_match = True
+                        matching_task_count += 1
+                    else:
+                        criteria_match = False
 
                     if criteria_match:
                         is_rule_applied = True
